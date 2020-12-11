4bba5733705dca9049d9f5233d095a4662b5cfdc
alexander <coding@aggrosoft.de>
2020-12-11 14:00:50 +0100

    bump version

--- copy_this/modules/agoxidbasket/metadata.php
+++ copy_this/modules/agoxidbasket/metadata.php
@@ -6,7 +6,7 @@ $aModule = array(
     'id'           => 'agoxidbasket',
     'title'        => 'Aggrosoft Warenkorb',
     'description'  => 'Verbessert Optik und Usability des standard Warenkorbs',
-    'version'      => '1.0.1',
+    'version'      => '1.0.2',
     'author'       => 'Aggrosoft GmbH',
     'controllers'  => [
 
